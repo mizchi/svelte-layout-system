08be5d29c863099a7a4b83530d784ccd0ee3cc25
mizchi <miz404@gmail.com>
2021-03-17 23:44:46 +0900

    Fix

--- packages/site/vite.config.js
+++ packages/site/vite.config.js
@@ -1,9 +1,6 @@
 import svelte from "rollup-plugin-svelte";
 import autoPreprocess from "svelte-preprocess";
-import alias from "@rollup/plugin-alias";
 import path from "path";
-
-const PROD = process.env.NODE_ENV === "production";
 
 export default {
   build: {
@@ -16,16 +13,6 @@ export default {
     },
   },
   plugins: [
-    alias({
-      entries: PROD
-        ? []
-        : [
-            {
-              find: "svelte-layout-system",
-              replacement: path.resolve(__dirname, "../src/index.ts"),
-            },
-          ],
-    }),
     svelte({
       emitCss: false,
       preprocess: autoPreprocess({
